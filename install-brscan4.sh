308e78569974f9192b40f1a794979b1de38becab
Keita <keita_nya@live.jp>
2022-07-07 00:19:49 -0400

    Update install-brscan4.sh

--- install-brscan4.sh
+++ install-brscan4.sh
@@ -22,7 +22,6 @@ rm -rf /tmp/brscan4-0.4.10-1.amd64.deb
 echo "----------------------------------------------------------------------------------"
 echo "You probably saw some errors but it's probably fine, probably."
 echo "You can run -> scanimage -L <- and your scanner should appear!"
-echo "If you don't need dpkg you can uninstall it using -> apk del dpkg <-"
 echo "-----------------------------------------------------------------------------------"
 echo "Press CTRL+C to close script because I don't know how to program an exit signal lol"
 echo "-----------------------------------------------------------------------------------"
