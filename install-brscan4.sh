3fdd7e629001dfb3a89c59b2e6e07d9e455fd4ec
Keita <keita_nya@live.jp>
2022-07-07 00:19:31 -0400

    Update install-brscan4.sh

--- install-brscan4.sh
+++ install-brscan4.sh
@@ -7,7 +7,7 @@ apk update && apk add --no-cache \
   dpkg \
   eudev
 
-# Temporarily add amd64 as a supported arch
+# Add amd64 as a supported arch
 sudo dpkg --add-architecture amd64
 
 # Download driver
@@ -16,16 +16,15 @@ curl -L https://github.com/x-keita/alpine-scripts/raw/main/drivers/scanner/brsca
 # Install driver
 dpkg -i /tmp/brscan4-0.4.10-1.amd64.deb
 
-# Remove amd64
-sudo dpkg --remove-architecture amd64
-
-echo "--------------------------------------------------------------------"
-echo "You probably saw some errors but it's probably fine, probably."
-echo "You can run -> scanimage -L <- and your scanner should appear!"
-echo "If you don't need dpkg you can uninstall it using -> apk del dpkg <-"
-echo "--------------------------------------------------------------------"
-
 # Cleanup
 rm -rf /tmp/brscan4-0.4.10-1.amd64.deb
 
-exit 0
+echo "----------------------------------------------------------------------------------"
+echo "You probably saw some errors but it's probably fine, probably."
+echo "You can run -> scanimage -L <- and your scanner should appear!"
+echo "If you don't need dpkg you can uninstall it using -> apk del dpkg <-"
+echo "-----------------------------------------------------------------------------------"
+echo "Press CTRL+C to close script because I don't know how to program an exit signal lol"
+echo "-----------------------------------------------------------------------------------"
+
+
