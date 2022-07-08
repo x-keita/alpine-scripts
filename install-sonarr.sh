48a9768a1b2d846266892c5061313843fa364f62
Keita <keita_nya@live.jp>
2022-07-08 16:14:42 -0400

    Remove updater script as it is no longer needed
    We can update using the built in method.

--- install-sonarr.sh
+++ install-sonarr.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-# Bash script for installing sonarr 
+# Bash script for installing sonarr on Alpine Linux
+
+sonarr_dir="/usr/lib/sonarr"
+sonarr_conf="/var/lib/sonarr"
 
 # Install required packages and dependencies
 apk update && apk add --no-cache \
@@ -14,8 +17,8 @@ apk add --no-cache --repository http://dl-cdn.alpinelinux.org/alpine/edge/testin
   mono
 
 # Create sonarr install folders
-mkdir -p /usr/lib/sonarr/bin
-mkdir -p /var/lib/sonarr
+mkdir -p $sonarr_dir/bin
+mkdir -p $sonarr_conf
 
 # Download sonarr
   if [ -z ${SONARR_VERSION+x} ]; then \
@@ -26,58 +29,33 @@ mkdir -p /var/lib/sonarr
     "https://download.sonarr.tv/v3/main/${SONARR_VERSION}/Sonarr.main.${SONARR_VERSION}.linux.tar.gz" && \
   tar xzf \
     /tmp/sonarr.tar.gz -C \
-    /usr/lib/sonarr/bin --strip-components=1
+    $sonarr_dir/bin --strip-components=1
 
 # Post install cleanup
-rm -rf /tmp/sonarr.tar.gz /usr/lib/sonarr/bin/Sonarr.Update
+rm -rf \
+  /tmp/sonarr.tar.gz \
+  $sonarr_dir/bin/Sonarr.Update
 
 # Create service
 curl -L https://raw.githubusercontent.com/x-keita/alpine-scripts/main/init.d/sonarr -o /etc/init.d/sonarr
 chmod 755 /etc/init.d/sonarr
+
 # Add service
 rc-update add sonarr default
 # Start server
 rc-service sonarr start
 
 # Set version variables
-#info="/usr/lib/sonarr/package_info"
-#cat <<  %%_INFO_%% > $info
-#PackageVersion=${SONARR_VERSION}
-#PackageAuthor=[x-keita](https://github.com/x-keita/alpine-scripts)
-#UpdateMethod=Script
-#Branch=main
-#%%_INFO_%%
+info="$sonarr_dir/package_info"
+package_ver=$(curl -sX GET http://services.sonarr.tv/v1/releases | jq -r '.[] | select(.branch=="main") | .version' | cut -b 1-5)
 
-# Add updater
-#curl -L https://github.com/x-keita/alpine-scripts/raw/main/updater/sonarr-update.sh -o /usr/lib/sonarr/updater
-#chmod 755 /usr/lib/sonarr/updater
+cat <<  %%_INFO_%% > $info
+# Do Not Edit
+PackageVersion=$package_ver
+PackageAuthor=[Team Sonarr](https://sonarr.tv) & Alpine script by: [x-keita](https://github.com/x-keita/alpine-scripts)
+ReleaseVersion=$SONARR_VERSION
+UpdateMethod=builtIn
+Branch=main
+%%_INFO_%%
 
-# Enable update script
-#sonarr_config="/var/lib/sonarr/config.xml"
-#
-#if [[ -f "${sonarr_config}" ]]; then
-#  sed -i 's%<UpdateMechanism>.*</UpdateMechanism>%<UpdateMechanism>Script</UpdateMechanism>%' "${sonarr_config}"
-#  sed -i 's%<UpdateScriptPath>.*</UpdateScriptPath>%<UpdateScriptPath>/usr/lib/sonarr/updater</UpdateScriptPath>%' "${sonarr_config}"
-#  if [[ $(grep -c "UpdateScriptPath" "${sonarr_config}") -eq 0 ]]; then
-#    sed -i 's%\(^</Config>$\)%  <UpdateMechanism>Script</UpdateMechanism>\n  <UpdateScriptPath>/usr/lib/sonarr/updater</UpdateScriptPath>%' "${sonarr_config}"
-#    echo -n "</Config>" >> "${sonarr_config}"
-#  fi
-#else
-#  {
-#    echo "<Config>"
-#    echo "  <LogLevel>info</LogLevel>"
-#    echo "  <EnableSsl>False</EnableSsl>"
-#    echo "  <Port>8989</Port>"
-#    echo "  <SslPort>9898</SslPort>"
-#    echo "  <BindAddress>*</BindAddress>"
-#    echo "  <ApiKey>ef8d989bcfce443fae07a408c4700fd1</ApiKey>"
-#    echo "  <AuthenticationMethod>None</AuthenticationMethod>"
-#    echo "  <UpdateMechanism>Script</UpdateMechanism>"
-#    echo "  <UpdateScriptPath>/usr/lib/sonarr/updater</UpdateScriptPath>"
-#    echo "  <Branch>main</Branch>"
-#    echo "  <SslCertHash></SslCertHash>"
-#    echo "</Config>"
-#  } > "${sonarr_config}"
-#fi
-#
-#exit 0
+exit 0
