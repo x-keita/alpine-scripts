f4bb981511a4c0eb3ffa620fa80904db8893dad9
Keita <keita_nya@live.jp>
2022-07-07 22:38:06 -0400

    Add sonarr updater

--- install-sonarr.sh
+++ install-sonarr.sh
@@ -42,4 +42,42 @@ rc-update add sonarr default
 # Start server
 rc-service sonarr start
 
-# Missing: Custom update script, for now just run the script again to update.
+# Set version variables
+echo "UpdateMethod=External\nBranch=main\nPackageVersion=${SONARR_VERSION}\nPackageAuthor=[x-keita](https://github.com/x-keita/alpine-scripts)" > /usr/lib/sonarr/package_info
+
+# Add updater
+updater="/usr/lib/sonarr/updater.sh"
+cat <<  %%_UPDATER_%% > $updater
+#!/bin/bash
+
+# Sonarr update script for Alpine Linux
+
+SONARR_VERSION=$(curl -sX GET http://services.sonarr.tv/v1/releases | jq -r ".[] | select(.branch==\"main\") | .version")
+CURRENT_VERSION=$(cat /usr/lib/sonarr/package_info | grep PackageVersion | tr -d 'PackageVersion=')
+
+if [ "$CURRENT_VERSION" = "$SONARR_VERSION ]; then
+    echo "Packages are the same version, no need to update."
+    exit 1
+else
+    # Download latest package
+    curl -o \
+      /tmp/sonarr.tar.gz -L \
+      "https://download.sonarr.tv/v3/main/${SONARR_VERSION}/Sonarr.main.${SONARR_VERSION}.linux.tar.gz" && \
+    tar xzf \
+      /tmp/sonarr.tar.gz -C \
+      /usr/lib/sonarr/bin --strip-components=1
+
+    # Remove integrated update service since we can't use it
+    rm -rf /usr/lib/sonarr/bin/Sonarr.Update && \
+
+    # Post install cleanup
+    rm -rf /tmp/sonarr.tar.gz
+
+# Recreate package_info with updated PackageVersion
+rm -rf /usr/lib/sonarr/package_info
+echo "UpdateMethod=External\nBranch=main\nPackageVersion=${SONARR_VERSION}\nPackageAuthor=[x-keita](https://github.com/x-keita/alpine-scripts)" > /usr/lib/sonarr/package_info
+fi
+exit 0
+%%_UPDATER_%%
+####
+chmod 755 $updater
