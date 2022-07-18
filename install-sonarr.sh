88a208d2fabb5ac491728f99535a34916c5fc6a5
Keita <keita_nya@live.jp>
2022-07-18 00:04:57 -0400

    Update install-sonarr.sh

--- install-sonarr.sh
+++ install-sonarr.sh
@@ -22,15 +22,14 @@ if [[ $prompt == "main" || $prompt == "Main" || $prompt == "MAIN" ]]
     PKG_BRANCH=develop
 fi
 PKG_VER=$(curl -sX GET http://services.sonarr.tv/v1/releases | jq -r ".[] | select(.branch==\"$PKG_BRANCH\") | .version")
-PKG_DIR="/opt/sonarr"
+PKG_DIR="/usr/lib/sonarr"
 PKG_CONF="/var/lib/sonarr"
-#PKG_INFO=$SONARR__DIR/package_info
-#VERSION=$(curl -sX GET http://services.sonarr.tv/v1/releases | jq -r '.[] | select(.branch==\"$PKG_BRANCH\") | .version' | cut -b 1-5)
+VERSION=$(curl -sX GET http://services.sonarr.tv/v1/releases | jq -r ".[] | select(.branch==\"$PKG_BRANCH\") | .version" | cut -b 1-5)
 # Userspace variables
 username=sonarr
 
 # Create sonarr install folders
-mkdir -p $PKG_DIR
+mkdir -p $PKG_DIR/bin
 mkdir -p $PKG_CONF
 
 # User management
@@ -40,12 +39,12 @@ adduser -u 1000 -H -D $username
   curl -L "https://download.sonarr.tv/v3/${PKG_BRANCH}/${PKG_VER}/Sonarr.main.${PKG_VER}.linux.tar.gz" -o /tmp/sonarr.tar.gz && \
   tar xzf \
     /tmp/sonarr.tar.gz -C \
-    $PKG_DIR --strip-components=1
+    $PKG_DIR/bin --strip-components=1
 
 # Post install cleanup
   rm -rf \
     /tmp/sonarr.tar.gz \
-    $PKG_DIR/Sonarr.Update
+    $PKG_DIR/bin/Sonarr.Update
 
 # Create service
 cat << EOF >> /etc/init.d/sonarr
@@ -53,7 +52,7 @@ cat << EOF >> /etc/init.d/sonarr
 
 name="sonarr"
 pidfile="/run/sonarr.pid"
-directory="$PKG_DIR"
+directory="$PKG_DIR/bin"
 command="/usr/bin/mono"
 command_args="--debug Sonarr.exe -nobrowser -data=$PKG_CONF"
 command_background=true
@@ -74,19 +73,17 @@ rc-update add sonarr default
 # Start server
 rc-service sonarr start
 
-#cat <<  %%_PKG_INFO_%% > $PKG_INFO
+cat << EOF >> $PKG_DIR/package_info
 # Do Not Edit
-#PackageVersion=$PKG_VER
-#PackageAuthor=[Team Sonarr](https://sonarr.tv) & Alpine Linux install script by: [x-keita](https://github.com/x-keita/alpine-scripts)
-#ReleaseVersion=$RELEASE_VERSION
-#UpdateMethod=builtIn
-#Branch=$BRANCH
-#%%_PKG_INFO_%%
+PackageVersion=$VERSION
+PackageAuthor=[Team Sonarr](https://sonarr.tv) & Alpine Linux install script by: [x-keita](https://github.com/x-keita/alpine-scripts)
+ReleaseVersion=$PKG_VER
+UpdateMethod=BuiltIn
+Branch=$PKG_BRANCH
+EOF
 
 # Add mono certs
 cert-sync /etc/ssl/certs/ca-certificates.crt
-
-# Script end text
 
     cat << EOF
 ------------------------------------------------------------------------------------
