7c296acd85697a402f080f062cda9b09d83c89c9
Keita <keita_nya@live.jp>
2022-07-18 00:44:32 -0400

    Update install-radarr.sh

--- install-radarr.sh
+++ install-radarr.sh
@@ -1,51 +1,60 @@
 #!/bin/bash
 
-# Bash script for installing prowlarr on Alpine Linux
+# Bash script for installing radarr on Alpine Linux
 
 # Install dependencies
 apk add -U --upgrade --no-cache \
-    curl \
-    jq \
-    icu-libs \
-    sqlite-libs
+  curl \
+  jq \
+  icu-libs \
+  sqlite-libs
 
 # Package variables
-PKG_BRANCH=develop
-PKG_VER=$(curl -sL "https://prowlarr.servarr.com/v1/update/${PKG_BRANCH}/changes?runtime=netcore&os=linuxmusl" | jq -r '.[0].version')
-PKG_DIR="/opt/prowlarr"
-PKG_CONF="/var/lib/prowlarr"
+read -r -p "Which radarr branch do you want to install? <master/develop> " prompt
+if [[ $prompt == "master" || $prompt == "Master" || $prompt == "MASTER" ]]
+  then
+    echo "Setting master as branch"
+    PKG_BRANCH=master
+  else
+    echo "Setting develop as branch"
+    PKG_BRANCH=develop
+fi
+PKG_VER=$(curl -sL "https://radarr.servarr.com/v1/update/${PKG_BRANCH}/changes?runtime=netcore&os=linuxmusl" | jq -r '.[0].version')
+PKG_DIR="/opt/radarr"
+PKG_CONF="/var/lib/radarr"
+VERSION=$(echo $PKG_VER | cut -b 1-5)
 # Userspace variables
-username=prowlarr
+username=radarr
 
-# Create install folder
-mkdir -p $PKG_DIR
+# Create radarr install folders
+mkdir -p $PKG_DIR/bin
 mkdir -p $PKG_CONF
 
 # User management
 adduser -u 1000 -H -D $username
 
-# Download and install latest prowlarr release
-  curl -L "https://prowlarr.servarr.com/v1/update/${PKG_BRANCH}/updatefile?version=${PKG_VER}&os=linuxmusl&runtime=netcore&arch=x64" -o /tmp/prowlarr.tar.gz  && \
+# Download and install latest radarr release
+  curl -L "https://radarr.servarr.com/v1/update/${PKG_BRANCH}/updatefile?version=${PKG_VER}&os=linuxmusl&runtime=netcore&arch=x64" -o /tmp/radarr.tar.gz && \
   tar xzf \
-    /tmp/prowlarr.tar.gz -C \
-    $PKG_DIR --strip-components=1
+    /tmp/radarr.tar.gz -C \
+    $PKG_DIR/bin --strip-components=1
 
 # Post install cleanup
   rm -rf \
-    /tmp/prowlarr.tar.gz
+    /tmp/radarr.tar.gz \
+    $PKG_DIR/bin/Radarr.Update
 
 # Create service
-cat << EOF >> /etc/init.d/prowlarr
+cat << EOF >> /etc/init.d/radarr
 #!/sbin/openrc-run
 
-name="prowlarr"
-pidfile="/run/prowlarr.pid"
-directory="$PKG_DIR"
-command="$PKG_DIR/Prowlarr"
+name="radarr"
+pidfile="/run/radarr.pid"
+directory="$PKG_DIR/bin"
+command="$PKG_DIR/bin/Radarr"
 command_args="-nobrowser -data=$PKG_CONF"
 command_background=true
 command_user="$username"
-command_group="$username"
 
 depend() {
     need net
@@ -55,16 +64,25 @@ EOF
 # Set permissions
 chown $username:$username -R $PKG_DIR
 chown $username:$username -R $PKG_CONF
-chmod 755 /etc/init.d/prowlarr
+chmod 755 /etc/init.d/radarr
 
-# Add service
-rc-update add prowlarr default
+# Add service to start on boot
+rc-update add radarr default
 # Start server
-rc-service prowlarr start
+rc-service radarr start
+
+cat << EOF >> $PKG_DIR/package_info
+# Do Not Edit
+PackageVersion=$VERSION
+PackageAuthor=[Team Radarr](https://radarr.video) & Alpine Linux install script by: [x-keita](https://github.com/x-keita/alpine-scripts)
+ReleaseVersion=$PKG_VER
+UpdateMethod=BuiltIn
+Branch=$PKG_BRANCH
+EOF
 
     cat << EOF
 ------------------------------------------------------------------------------------
-Installed! Prowlarr runs on localhost:9696 by default. 
+Installed! Radarr runs on localhost:7878 by default. 
 Also you can update directly from the application using the BuiltIn method and/or
 change branch to develop without any issues.
 ------------------------------------------------------------------------------------
